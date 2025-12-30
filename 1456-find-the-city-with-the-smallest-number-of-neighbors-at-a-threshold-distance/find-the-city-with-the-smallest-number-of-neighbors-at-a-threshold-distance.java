1f4b302505537f38bb0fa477dd458866cb4b33ed
Arshita1510-coder <arshita1510@gmail.com>
2025-12-30 22:42:09 +0530

    Time: 9 ms (79.89%) | Memory: 46.8 MB (26.34%) - LeetSync

--- 1456-find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance/find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance.java
+++ 1456-find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance/find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance.java
@@ -1,27 +1,48 @@
 class Solution {
         public int findTheCity(int n, int[][] edges, int distanceThreshold) {
-        int[][] dis = new int[n][n];
-        int res = 0, smallest = n;
-        for (int[] row : dis)
-            Arrays.fill(row, 10001);
-        for (int[] e : edges)
-            dis[e[0]][e[1]] = dis[e[1]][e[0]] = e[2];
-        for (int i = 0; i < n; ++i)
-            dis[i][i] = 0;
-        for (int k = 0; k < n; ++k)
-            for (int i = 0; i < n; ++i)
-                for (int j = 0; j < n; ++j)
-                    dis[i][j] = Math.min(dis[i][j], dis[i][k] + dis[k][j]);
-        for (int i = 0; i < n; i++) {
-            int count = 0;
-            for (int j = 0; j < n; ++j)
-                if (dis[i][j] <= distanceThreshold)
-                    ++count;
-            if (count <= smallest) {
-                res = i;
-                smallest = count;
-            }
+             int[][]dist=new int[n][n];
+             for(int i=0;i<n;i++){
+                for(int j=0;j<n;j++){
+                  dist[i][j]=Integer.MAX_VALUE;
+                }
+             
+             dist[i][i]=0;
+             }
+             for(int[]e:edges){
+                int u=e[0];
+                int v=e[1];
+                int wt=e[2];
+                dist[u][v]=wt;
+                dist[v][u]=wt;
+             }
+             for(int k=0;k<n;k++){
+                for(int i=0;i<n;i++){
+                    for(int j=0;j<n;j++){
+                        if(dist[i][k]==Integer.MAX_VALUE||dist[k][j]==Integer.MAX_VALUE){
+                            continue;
+                        }
+                        dist[i][j]=Math.min(dist[i][j],dist[i][k]+dist[k][j]);
+                    }
+                }
+             }
+             int minCount=n;
+             int cityNo=-1;
+             for(int city=0;city<n;city++){
+                int Count=0;
+                for(int adj=0;adj<n;adj++){
+                    if(dist[city][adj]<=distanceThreshold){
+                        Count++;
+                    }
+                }
+                if(Count<=minCount){
+                    minCount=Count;
+                    cityNo=city;
+                }
+             }
+             return cityNo;
         }
-        return res;
-    }
 }
+
+             
+
+             

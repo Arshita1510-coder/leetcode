f6a4c60478bbfddf79aa5ff5931e155f61867e36
Arshita1510-coder <arshita1510@gmail.com>
2026-01-06 03:29:25 +0530

    Time: 20 ms (15.36%) | Memory: 66.2 MB (57.11%) - LeetSync

--- 1442-number-of-operations-to-make-network-connected/number-of-operations-to-make-network-connected.java
+++ 1442-number-of-operations-to-make-network-connected/number-of-operations-to-make-network-connected.java
@@ -1,33 +1,64 @@
-class Solution {
-    int extraWire;
-    public int makeConnected(int n, int[][] connections) {
-        extraWire = 0;
-        ArrayList<Integer>[] adj = new ArrayList[n];
-        for(int i = 0; i < n; i++) adj[i] = new ArrayList<>();
-        for(int[] conn : connections) {
-            adj[conn[0]].add(conn[1]);
-            adj[conn[1]].add(conn[0]);
-        }
 
-        int comp = 0;
-        boolean[] visit = new boolean[n];
-        for(int i = 0;i < n;i++){
-            if(!visit[i]){
-                comp++;
-                dfs(adj, i, -1, visit);
-            }
-        }
+   
+   
+class DisjointSet{
+     List<Integer> rank = new ArrayList<>();
+    List<Integer> parent = new ArrayList<>();
+    List<Integer>size=new ArrayList<>();
 
-        if(comp-1 > extraWire/2) return -1;
-        return comp-1;
+    public DisjointSet(int n) {
+        for (int i = 0; i <= n; i++) {
+            rank.add(0);
+            parent.add(i);
+            size.add(1);
+        }
     }
-    public void dfs(ArrayList<Integer>[] adj, int node, int pre, boolean[] visit){
-        visit[node] = true;
-        for(int nxt: adj[node]){
-            if(!visit[nxt]){
-                dfs(adj, nxt, node, visit);
-            }
-            else if(nxt != pre) extraWire++;
+
+    public int findUPar(int node) {
+        if (node == parent.get(node)) {
+            return node;
         }
+        int ulp = findUPar(parent.get(node));
+        parent.set(node, ulp);
+        return parent.get(node);
+    }
+
+   
+    public void unionBySize(int u,int v){
+        int ulp_u=findUPar(u);
+        int ulp_v=findUPar(v);
+        if(ulp_u==ulp_v) return;
+        if(size.get(ulp_u)<size.get(ulp_v)){
+            parent.set(ulp_u,ulp_v);
+            size.set(ulp_v,size.get(ulp_v)+size.get(ulp_u));
+        }else{
+            parent.set(ulp_v,ulp_u);
+            size.set(ulp_u,size.get(ulp_u)+size.get(ulp_v));
+        }
+    }
+}
+class Solution{
+    public int makeConnected(int n, int[][] connections) {
+        DisjointSet ds=new DisjointSet(n);
+        int cntExtras=0;
+        int m=connections.length;
+        for(int i=0;i<m;i++){
+            int u=connections[i][0];
+            int v=connections[i][1];
+            if(ds.findUPar(u)==ds.findUPar(v)){
+                cntExtras++;
+
+            }else{
+                ds.unionBySize(u,v);
+
+            }
+        }
+        int cntC=0;
+        for(int i=0;i<n;i++){
+            if(ds.parent.get(i)==i) cntC++;
+        }
+        int ans=cntC-1;
+        if(cntExtras>=ans) return ans;
+        return -1;
     }
 }

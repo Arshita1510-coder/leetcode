269d2070fa46b2f16b3d99c73586a8b0bd04c830
Arshita1510-coder <arshita1510@gmail.com>
2026-01-20 01:47:05 +0530

    Time: 0 ms (100.00%) | Memory: 47.6 MB (13.63%) - LeetSync

--- 203-remove-linked-list-elements/remove-linked-list-elements.java
+++ 203-remove-linked-list-elements/remove-linked-list-elements.java
@@ -10,10 +10,13 @@
  */
 class Solution {
     public ListNode removeElements(ListNode head, int val) {
-       if(head==null) return null;
-       head.next=removeElements(head.next,val);
-       return head.val==val?head.next:head;
+        if(head == null) return head;
 
-        
+        head.next = removeElements(head.next, val);
+        if(head.val == val){
+            return head.next;
+        }else {
+            return head;
+        }
     }
 }

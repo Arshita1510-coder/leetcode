a3791f69618f3de6e4d3411f8e8ea793ab47d2d8
Arshita1510-coder <arshita1510@gmail.com>
2026-01-26 23:24:15 +0530

    Time: 0 ms (100.00%) | Memory: 42.9 MB (34.93%) - LeetSync

--- 812-rotate-string/rotate-string.java
+++ 812-rotate-string/rotate-string.java
@@ -3,13 +3,8 @@ class Solution {
         if(s.length()!=goal.length()){
             return false;
         }
-        for(int i=0;i<s.length();i++){
-            String rotated=s.substring(i)+s.substring(0,i);
-            if(rotated.equals(goal)){
-                return true;
-            }
-        }
-        return false;
+        String doubledS=s+s;
+        return doubledS.contains(goal);
         
     }
 }

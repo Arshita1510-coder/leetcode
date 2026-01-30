b2ec17cafb1496ec8e20a5c0086b2b85b6a8b00b
Arshita1510-coder <arshita1510@gmail.com>
2026-01-30 20:36:59 +0530

    Time: 1 ms (99.64%), Space: 43.7 MB (71.46%) - LeetHub

--- 0008-string-to-integer-atoi/0008-string-to-integer-atoi.java
+++ 0008-string-to-integer-atoi/0008-string-to-integer-atoi.java
@@ -1,38 +1,23 @@
 class Solution {
-    static final int INT_MIN_VAL = -2147483648;
-    static final int INT_MAX_VAL = 2147483647;
-
-    // Recursive helper
-    static int helper(String s, int i, long num, int sign) {
-        // Base case: end or non-digit
-        if (i >= s.length() || !Character.isDigit(s.charAt(i)))
-            return (int)(sign * num);
-
-        // Update num
-        num = num * 10 + (s.charAt(i) - '0');
-
-        // Clamp overflow
-        if (sign * num <= INT_MIN_VAL) return INT_MIN_VAL;
-        if (sign * num >= INT_MAX_VAL) return INT_MAX_VAL;
-
-        // Recurse
-        return helper(s, i + 1, num, sign);
-    }
-
-    static int myAtoi(String s) {
-        int i = 0;
-
-        // Skip whitespaces
-        while (i < s.length() && s.charAt(i) == ' ') i++;
-
-        // Handle sign
-        int sign = 1;
-        if (i < s.length() && (s.charAt(i) == '+' || s.charAt(i) == '-')) {
-            sign = (s.charAt(i) == '-') ? -1 : 1;
+    public int myAtoi(String s){
+        int i=0,sign=1;
+        long res=0;
+        while(i<s.length()&&s.charAt(i)==' ') i++;
+        if(i==s.length()) return 0;
+        if(s.charAt(i)=='-'){
+            sign=-1;
+            i++;
+        }else if(s.charAt(i)=='+'){
             i++;
         }
-
-        // Recursive helper
-        return helper(s, i, 0, sign);
+        while(i<s.length()&&Character.isDigit(s.charAt(i))){
+            res=res*10+(s.charAt(i)-'0');
+            if(sign*res>Integer.MAX_VALUE) return Integer.MAX_VALUE;
+            if(sign*res<Integer.MIN_VALUE) return Integer.MIN_VALUE;
+            i++;
+        }
+        return (int)(sign*res);
+    
+    
     }
 }

1c1f1ac3974dabcf25e5ba4f14432d254717f760
Arshita1510-coder <arshita1510@gmail.com>
2026-02-14 19:36:57 +0530

    Time: 1 ms (99.69%) | Memory: 46.5 MB (91.75%) - LeetSync

--- 51-n-queens/n-queens.java
+++ 51-n-queens/n-queens.java
@@ -1,55 +1,35 @@
 class Solution {
-    public boolean isSafe(int row,int col,char[][]board,int n){
-        int duprow=row;
-        int dupcol=col;
-        while(row>=0&&col>=0){
-            if(board[row][col]=='Q') return false;
-            row--;
-            col--;
-        }
-        col=dupcol;
-        row=duprow;
-        while(col>=0){
-            if(board[row][col]=='Q') return false;
-            col--;
-        }
-        row=duprow;
-        col=dupcol;
-        while(row<n&&col>=0){
-            if(board[row][col]=='Q') return false;
-            row++;
-            col--;
-        }
-        return true;
-        
-    }
-    public void solve(int col,char[][]board,List<List<String>>ans,int n){
+    public void solve(int col,char[][]board,int n,int[]leftRow,int[]upperDiagonal,int[]lowerDiagonal,List<List<String>>res){
         if(col==n){
             List<String>temp=new ArrayList<>();
             for(int i=0;i<n;i++){
                 temp.add(new String(board[i]));
             }
-            ans.add(temp);
+            res.add(temp);
             return;
         }
         for(int row=0;row<n;row++){
-            if(isSafe(row,col,board,n)){
+            if(leftRow[row]==0&&lowerDiagonal[row+col]==0&&upperDiagonal[n-1+col-row]==0){
                 board[row][col]='Q';
-                solve(col+1,board,ans,n);
+                leftRow[row]=1;
+                lowerDiagonal[row+col]=1;
+                upperDiagonal[n-1+col-row]=1;
+                solve(col+1,board,n,leftRow,upperDiagonal,lowerDiagonal,res);
                 board[row][col]='.';
+                leftRow[row]=0;
+                lowerDiagonal[row+col]=0;
+                upperDiagonal[n-1+col-row]=0;
             }
         }
-
     }
-    public List<List<String>> solveNQueens(int n) {
-        List<List<String>>ans=new ArrayList<>();
+    public List<List<String>>solveNQueens(int n){
+        List<List<String>>res=new ArrayList<>();
         char[][]board=new char[n][n];
-        for(int i=0;i<n;i++){
-            Arrays.fill(board[i],'.');
-        }
-        solve(0,board,ans,n);
-        return ans;
-
-        
+        for(char[]row:board) Arrays.fill(row,'.');
+        int[]leftRow=new int[n];
+        int[]lowerDiagonal=new int[2*n-1];
+        int[]upperDiagonal=new int[2*n-1];
+        solve(0,board,n,leftRow,upperDiagonal,lowerDiagonal,res);
+        return res;
     }
 }

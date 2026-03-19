ecfa0d96845ac343979d4323d6d202a71a19db79
Arshita1510-coder <arshita1510@gmail.com>
2026-03-20 02:55:22 +0530

    Time: 0 ms (100%), Space: 43.5 MB (58.97%) - LeetHub

--- 0063-unique-paths-ii/0063-unique-paths-ii.java
+++ 0063-unique-paths-ii/0063-unique-paths-ii.java
@@ -1,32 +1,38 @@
 class Solution {
-    private int func(int m,int n,int[][]obstacleGrid,int[][]dp){
-       for(int i=0;i<n;i++){
-        for(int j=0;j<m;j++){
-            if(obstacleGrid[i][j]==1){
-                dp[i][j]=0;
-                continue;
+
+    public int uniquePathsWithObstacles(int[][] obstacleGrid) {
+        int m = obstacleGrid.length;      // rows
+        int n = obstacleGrid[0].length;   // cols
+
+        // Edge case: starting cell blocked
+        if (obstacleGrid[0][0] == 1) return 0;
+
+        int[][] dp = new int[m][n];
+
+        // Starting point
+        dp[0][0] = 1;
+
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+
+                // If obstacle → no paths
+                if (obstacleGrid[i][j] == 1) {
+                    dp[i][j] = 0;
+                    continue;
+                }
+
+                // Skip start cell (already set)
+                if (i == 0 && j == 0) continue;
+
+                int up = 0, left = 0;
+
+                if (i > 0) up = dp[i - 1][j];
+                if (j > 0) left = dp[i][j - 1];
+
+                dp[i][j] = up + left;
             }
-            if(i==0&&j==0){
-                dp[i][j]=1;
-                continue;
-            }
-            int up=0;
-            int left=0;
-            if(i>0) up=dp[i-1][j];
-            if(j>0) left=dp[i][j-1];
-            dp[i][j]=up+left;
         }
 
-       }
-       return dp[n-1][m-1];
-    }
-    public int uniquePathsWithObstacles(int[][] obstacleGrid) {
-        int m=obstacleGrid.length;
-        int n=obstacleGrid[0].length;
-        int[][]dp=new int[m][n];
-       
-        return func(m,n,obstacleGrid,dp);
-
-        
+        return dp[m - 1][n - 1];
     }
 }

9caca3451113495a5126bb0847fd63436f3315c6
Arshita1510-coder <arshita1510@gmail.com>
2026-03-29 17:34:32 +0530

    Time: 89 ms (24.93%) | Memory: 75.9 MB (15.40%) - LeetSync

--- 416-partition-equal-subset-sum/partition-equal-subset-sum.java
+++ 416-partition-equal-subset-sum/partition-equal-subset-sum.java
@@ -1,42 +1,16 @@
 class Solution {
-     public boolean subsetSumToK(int n, int k, int[] nums) {
-        // Initialize previous row of DP table with false
-        boolean[] prev = new boolean[k + 1];
-
-        // Base case: sum 0 can always be formed with empty subset
-        prev[0] = true;
-
-        // Base case: if first element <= k, mark true
-        if (nums[0] <= k) {
-            prev[nums[0]] = true;
+     public boolean subsetSumToK(int ind, int target, int[] nums,int[][]dp) {
+        if(target==0) return true;
+        if(ind==0) return (nums[0]==target);
+        if(dp[ind][target]!=-1) return dp[ind][target]==1;
+        boolean notTake=subsetSumToK(ind-1,target,nums,dp);
+        boolean take=false;
+        if(nums[ind]<=target){
+            take=subsetSumToK(ind-1,target-nums[ind],nums,dp);
         }
-
-        // Iterate over elements starting from second
-        for (int ind = 1; ind < n; ind++) {
-            // Current row of DP table
-            boolean[] cur = new boolean[k + 1];
-            cur[0] = true; // sum 0 always possible
-
-            for (int target = 1; target <= k; target++) {
-                // Option 1: not take current element
-                boolean notTaken = prev[target];
-
-                // Option 2: take current element if possible
-                boolean taken = false;
-                if (nums[ind] <= target) {
-                    taken = prev[target - nums[ind]];
-                }
-
-                // Store true if either option is true
-                cur[target] = notTaken || taken;
-            }
-            // Move current row to previous for next iteration
-            prev = cur;
-        }
-
-        // Return if sum k is possible using all elements
-        return prev[k];
-    }
+        dp[ind][target]=(notTake||take)?1:0;
+        return notTake||take;
+     }
 
     public boolean canPartition(int[] nums) {
         int n=nums.length;
@@ -44,7 +18,11 @@ class Solution {
         for(int i=0;i<n;i++) totalSum+=nums[i];
         if(totalSum%2==1) return false;
         int target=totalSum/2;
-        return subsetSumToK(n,target,nums);
+        int[][]dp=new int[n][target+1];
+        for(int row[]:dp){
+            Arrays.fill(row,-1);
+        }
+        return subsetSumToK(n-1,target,nums,dp);
 
         
     }

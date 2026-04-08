aa5e5b5776d2c9fc043645750ef20c89dc4fa818
Arshita1510-coder <arshita1510@gmail.com>
2026-04-09 01:58:46 +0530

    Time: 17 ms (47.08%), Space: 46.9 MB (35.25%) - LeetHub

--- 0322-coin-change/0322-coin-change.java
+++ 0322-coin-change/0322-coin-change.java
@@ -1,39 +1,31 @@
 class Solution {
-   public int f(int ind,int T,int[]coins,int[][]dp){
-    //base case
-    if(ind==0){
-        if(T%coins[0]==0){
-            return T/coins[0];
-        }else{
-            return (int)1e9;
-        }
-    }
-    if(dp[ind][T]!=-1){
-        return dp[ind][T];
-    }
-    int notTake=f(ind-1,T,coins,dp);
-    int take=(int)1e9;
-    if(coins[ind]<=T){
-        take=1+f(ind,T-coins[ind],coins,dp);
-    }
-    return dp[ind][T]=Math.min(take,notTake);
-   }
-        
-    
-
+   
+      
     public int coinChange(int[] coins, int amount) {
         int n=coins.length;
         int[][]dp=new int[n][amount+1];
         for(int i=0;i<n;i++){
             for(int j=0;j<=amount;j++){
-                dp[i][j]=-1;
+                dp[i][j]=0;
             }
         }
-        int ans=f(n-1,amount,coins,dp);
-        if(ans>=(int)1e9)
-           return -1;
-
-        return ans;   
+        for(int T=0;T<=amount;T++){
+            if(T%coins[0]==0) dp[0][T]=T/coins[0];
+            else dp[0][T]=(int)1e9;
+        }
+        for(int ind=1;ind<n;ind++){
+            for(int T=0;T<=amount;T++){
+                int notTake=dp[ind-1][T];
+                int take=Integer.MAX_VALUE;
+                if(coins[ind]<=T){
+                    take=1+dp[ind][T-coins[ind]];
+                }
+                dp[ind][T]=Math.min(take,notTake);
+            }
+        }
+      int ans=dp[n-1][amount];
+      if(ans>=(int)1e9) return -1;
+      return ans;
 
         
     }

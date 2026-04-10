7802b93a6e680905e825b37995e1875990ba4c5c
Arshita1510-coder <arshita1510@gmail.com>
2026-04-11 01:22:29 +0530

    Time: 14 ms (90.57%), Space: 46.6 MB (40.19%) - LeetHub

--- 0322-coin-change/0322-coin-change.java
+++ 0322-coin-change/0322-coin-change.java
@@ -2,28 +2,27 @@ class Solution {
    
       
     public int coinChange(int[] coins, int amount) {
+        int prev[]=new int[amount+1];
+        int curr[]=new int[amount+1];
         int n=coins.length;
-        int[][]dp=new int[n][amount+1];
-        for(int i=0;i<n;i++){
-            for(int j=0;j<=amount;j++){
-                dp[i][j]=0;
-            }
-        }
+       
+       
         for(int T=0;T<=amount;T++){
-            if(T%coins[0]==0) dp[0][T]=T/coins[0];
-            else dp[0][T]=(int)1e9;
+            if(T%coins[0]==0) prev[T]=T/coins[0];
+            else prev[T]=(int)1e9;
         }
         for(int ind=1;ind<n;ind++){
             for(int T=0;T<=amount;T++){
-                int notTake=dp[ind-1][T];
+                int notTake=prev[T];
                 int take=Integer.MAX_VALUE;
                 if(coins[ind]<=T){
-                    take=1+dp[ind][T-coins[ind]];
+                    take=1+curr[T-coins[ind]];
                 }
-                dp[ind][T]=Math.min(take,notTake);
+                curr[T]=Math.min(take,notTake);
             }
+            prev=curr;
         }
-      int ans=dp[n-1][amount];
+      int ans=prev[amount];
       if(ans>=(int)1e9) return -1;
       return ans;
 

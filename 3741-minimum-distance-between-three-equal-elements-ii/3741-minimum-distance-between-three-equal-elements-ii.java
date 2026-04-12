8679c23befb7cd402766bfb4c1a512eaef4f791d
Arshita1510-coder <arshita1510@gmail.com>
2026-04-12 13:08:35 +0530

    Time: 127 ms (33.08%), Space: 274.3 MB (39.51%) - LeetHub

--- 3741-minimum-distance-between-three-equal-elements-ii/3741-minimum-distance-between-three-equal-elements-ii.java
+++ 3741-minimum-distance-between-three-equal-elements-ii/3741-minimum-distance-between-three-equal-elements-ii.java
@@ -2,30 +2,27 @@ import java.util.*;
 
 class Solution {
     public int minimumDistance(int[] nums) {
-        Map<Integer, List<Integer>> mp = new HashMap<>();
-        int n = nums.length;
-
-        for (int i = 0; i < n; i++) {
-            mp.computeIfAbsent(nums[i], k -> new ArrayList<>()).add(i);
+        Map<Integer,List<Integer>>mp=new HashMap<>();
+        int n=nums.length;
+        for(int i=0;i<n;i++){
+            if(!mp.containsKey(nums[i])){
+                mp.put(nums[i],new ArrayList<>());
+            }
+            mp.get(nums[i]).add(i);
         }
-
-        int mini = Integer.MAX_VALUE;
-
-        for (List<Integer> temp : mp.values()) {
-            int m = temp.size();
-
-            if (m >= 3) {
-                for (int i = 0; i < m - 2; i++) {
-                    int a = temp.get(i);
-                    int b = temp.get(i + 1);
-                    int c = temp.get(i + 2);
-
-                    int diff = 2 * (c - a);
-                    mini = Math.min(mini, diff);
+        int mini=Integer.MAX_VALUE;
+        for(List<Integer>temp:mp.values()){
+            int m=temp.size();
+            if(m>=3){
+                for(int i=0;i<m-2;i++){
+                    int a=temp.get(i);
+                    int b=temp.get(i+1);
+                    int c=temp.get(i+2);
+                    int diff=2*(c-a);
+                    mini=Math.min(mini,diff);
                 }
             }
         }
-
-        return mini == Integer.MAX_VALUE ? -1 : mini;
+        return mini==Integer.MAX_VALUE?-1:mini;
     }
 }

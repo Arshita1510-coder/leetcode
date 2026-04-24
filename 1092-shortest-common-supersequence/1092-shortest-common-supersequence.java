d99906c78f56da51d198695664cd08bc629db898
Arshita1510-coder <arshita1510@gmail.com>
2026-04-25 01:43:17 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 1092-shortest-common-supersequence/1092-shortest-common-supersequence.java
+++ 1092-shortest-common-supersequence/1092-shortest-common-supersequence.java
@@ -2,16 +2,20 @@ class Solution {
     public String shortestCommonSupersequence(String str1, String str2) {
         int n=str1.length();
         int m=str2.length();
-        int[][]dp=new int[n+1][m+1];
+        int[]prev=new int[m+1];
+        int[]curr=new int[m+1];
         for(int i=1;i<=n;i++){
             for(int j=1;j<=m;j++){
                 if(str1.charAt(i-1)==str2.charAt(j-1)){
-                    dp[i][j]=1+dp[i-1][j-1];
+                    curr[j]=1+prev[j-1];
                 }
                 else{
-                    dp[i][j]=Math.max(dp[i-1][j],dp[i][j-1]);
+                    curr[j]=Math.max(prev[j],curr[j-1]);
                 }
             }
+            int[]temp=prev;
+            prev=curr;
+            curr=temp;
         }
         StringBuilder ans=new StringBuilder();
         int i=n,j=m;
@@ -20,7 +24,7 @@ class Solution {
                 ans.append(str1.charAt(i-1));
                 i--;
                 j--;
-            }else if(dp[i-1][j]>dp[i][j-1]){
+            }else if(prev[j]>curr[j-1]){
                 ans.append(str1.charAt(i-1));
                 i--;
             }else{

63b931b84079a053b1a3e52e7031aa8a04b559d6
Arshita1510-coder <arshita1510@gmail.com>
2026-04-27 04:03:02 +0530

    Time: 18 ms (80.04%) | Memory: 138.1 MB (68.84%) - LeetSync

--- 1663-detect-cycles-in-2d-grid/detect-cycles-in-2d-grid.java
+++ 1663-detect-cycles-in-2d-grid/detect-cycles-in-2d-grid.java
@@ -6,47 +6,37 @@ class Solution {
     public boolean containsCycle(char[][] grid) {
         int m = grid.length;
         int n = grid[0].length;
-        
-        boolean[][] vis = new boolean[m][n];
-        
-        for(int i = 0; i < m; i++){
-            for(int j = 0; j < n; j++){
+        boolean[][]vis=new boolean[m][n];
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
                 if(!vis[i][j]){
-                    if(dfs(grid, vis, i, j, -1, -1))
+                    if(dfs(grid,vis,i,j,-1,-1))
                         return true;
                 }
             }
         }
-        
         return false;
+        
     }
-    
-    public boolean dfs(char[][] grid, boolean[][] vis, int r, int c, int pr, int pc){
-        vis[r][c] = true;
-        
-        for(int k = 0; k < 4; k++){
-            int nr = r + dr[k];
-            int nc = c + dc[k];
-            
-            // boundary check
-            if(nr < 0 || nc < 0 || nr >= grid.length || nc >= grid[0].length)
+    public boolean dfs(char[][]grid,boolean[][]vis,int r,int c,int pr,int pc){
+        vis[r][c]=true;
+        for(int k=0;k<4;k++){
+            int nr=r+dr[k];
+            int nc=c+dc[k];
+            if(nr<0||nc<0||nr>=grid.length||nc>=grid[0].length){
                 continue;
-            
-            // same character check
-            if(grid[nr][nc] != grid[r][c])
-                continue;
-            
-            // cycle condition
-            if(vis[nr][nc] && !(nr == pr && nc == pc))
-                return true;
-            
-            // DFS call
-            if(!vis[nr][nc]){
-                if(dfs(grid, vis, nr, nc, r, c))
-                    return true;
             }
+            if(grid[nr][nc]!=grid[r][c]){
+                continue;
+            }
+            if(vis[nr][nc]&&!(nr==pr&&nc==pc))
+                 return true;
+
+             if(!vis[nr][nc]){
+                if(dfs(grid,vis,nr,nc,r,c))
+                    return true;
+             }  
         }
-        
         return false;
     }
 }

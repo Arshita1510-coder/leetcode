197dfd29afb91bfec276efa44343e8c4a25d0195
Arshita1510-coder <arshita1510@gmail.com>
2026-05-19 23:20:37 +0530

    Time: 15 ms (66.04%), Space: 60.4 MB (23.48%) - LeetHub

---
+++ 0044-wildcard-matching/0044-wildcard-matching.java
@@ -0,0 +1,31 @@
+class Solution {
+    public boolean wildCardMatching(String p,String s,int i,int j,Boolean[][]dp){
+        if(i<0&&j<0) return true;
+        if(i<0&&j>=0) return false;
+        if(j<0&&i>=0){
+            for(int k=0;k<=i;k++){
+                if(p.charAt(k)!='*')
+                   return false;
+            }
+            return true;
+        }
+        if(dp[i][j]!=null) return dp[i][j];
+
+        if(p.charAt(i)==s.charAt(j)||p.charAt(i)=='?'){
+             return dp[i][j]= wildCardMatching(p,s,i-1,j-1,dp);
+
+        }if(p.charAt(i)=='*'){
+             return dp[i][j]= (wildCardMatching(p,s,i-1,j,dp)||wildCardMatching(p,s,i,j-1,dp));
+        }
+        return dp[i][j]=false;
+    }
+    public boolean isMatch(String s, String p) {
+        int n=p.length();
+        int m=s.length();
+        Boolean[][]dp=new Boolean[n][m];
+        
+        return wildCardMatching(p,s,n-1,m-1,dp);
+
+        
+    }
+}

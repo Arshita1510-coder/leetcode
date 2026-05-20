bdf7fba4de2fd8efeef57b1d66af1d40e818d488
Arshita1510-coder <arshita1510@gmail.com>
2026-05-20 15:27:18 +0530

    Time: 63 ms (11.11%), Space: 64.2 MB (5.95%) - LeetHub

--- 0044-wildcard-matching/0044-wildcard-matching.java
+++ 0044-wildcard-matching/0044-wildcard-matching.java
@@ -1,31 +1,39 @@
 class Solution {
-    public boolean wildCardMatching(String p,String s,int i,int j,Boolean[][]dp){
-        if(i==00&&j==00) return true;
-        if(i==0&&j>0) return false;
-        if(j==0&&i>0){
-            for(int k=1;k<=i;k++){
-                if(p.charAt(k-1)!='*')
-                   return false;
-            }
-            return true;
-        }
-        if(dp[i][j]!=null) return dp[i][j];
-
-        if(p.charAt(i-1)==s.charAt(j-1)||p.charAt(i-1)=='?'){
-             return dp[i][j]= wildCardMatching(p,s,i-1,j-1,dp);
-
-        }if(p.charAt(i-1)=='*'){
-             return dp[i][j]= (wildCardMatching(p,s,i-1,j,dp)||wildCardMatching(p,s,i,j-1,dp));
-        }
-        return dp[i][j]=false;
-    }
+    
     public boolean isMatch(String s, String p) {
         int n=p.length();
         int m=s.length();
         Boolean[][]dp=new Boolean[n+1][m+1];
-        
-        return wildCardMatching(p,s,n,m,dp);
+        dp[0][0]=true;
+        for(int j=1;j<=m;j++){
+            dp[0][j]=false;
+        }
+        for(int i=1;i<=n;i++){
+            boolean flag=true;
+            for(int k=1;k<=i;k++){
+                if(p.charAt(k-1)!='*'){
+                    flag=false;;
+                    break;
+                }
+                
+            }
+            dp[i][0]=flag;
+        }
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=m;j++){
+                if(p.charAt(i-1)==s.charAt(j-1)||p.charAt(i-1)=='?'){
+                    dp[i][j]=dp[i-1][j-1];
+                }else if(p.charAt(i-1)=='*'){
+                    dp[i][j]=dp[i-1][j]||dp[i][j-1];
+                }else{
+                     dp[i][j]=false;
+                }
+            }
+        }
+        return dp[n][m];
 
+        
+       
         
     }
 }

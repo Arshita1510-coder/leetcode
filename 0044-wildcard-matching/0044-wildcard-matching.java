8843133296bf995e63da6968a8d540d27be11712
Arshita1510-coder <arshita1510@gmail.com>
2026-05-20 14:28:10 +0530

    Time: 14 ms (66.84%), Space: 63.9 MB (7.49%) - LeetHub

--- 0044-wildcard-matching/0044-wildcard-matching.java
+++ 0044-wildcard-matching/0044-wildcard-matching.java
@@ -1,20 +1,20 @@
 class Solution {
     public boolean wildCardMatching(String p,String s,int i,int j,Boolean[][]dp){
-        if(i<0&&j<0) return true;
-        if(i<0&&j>=0) return false;
-        if(j<0&&i>=0){
-            for(int k=0;k<=i;k++){
-                if(p.charAt(k)!='*')
+        if(i==00&&j==00) return true;
+        if(i==0&&j>0) return false;
+        if(j==0&&i>0){
+            for(int k=1;k<=i;k++){
+                if(p.charAt(k-1)!='*')
                    return false;
             }
             return true;
         }
         if(dp[i][j]!=null) return dp[i][j];
 
-        if(p.charAt(i)==s.charAt(j)||p.charAt(i)=='?'){
+        if(p.charAt(i-1)==s.charAt(j-1)||p.charAt(i-1)=='?'){
              return dp[i][j]= wildCardMatching(p,s,i-1,j-1,dp);
 
-        }if(p.charAt(i)=='*'){
+        }if(p.charAt(i-1)=='*'){
              return dp[i][j]= (wildCardMatching(p,s,i-1,j,dp)||wildCardMatching(p,s,i,j-1,dp));
         }
         return dp[i][j]=false;
@@ -22,9 +22,9 @@ class Solution {
     public boolean isMatch(String s, String p) {
         int n=p.length();
         int m=s.length();
-        Boolean[][]dp=new Boolean[n][m];
+        Boolean[][]dp=new Boolean[n+1][m+1];
         
-        return wildCardMatching(p,s,n-1,m-1,dp);
+        return wildCardMatching(p,s,n,m,dp);
 
         
     }

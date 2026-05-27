6a698723ea121b7ba138f11374231c5a9f1d106b
Arshita1510-coder <arshita1510@gmail.com>
2026-05-27 15:02:43 +0530

    Time: 1 ms (68.97%), Space: 43.8 MB (18.33%) - LeetHub

--- 0309-best-time-to-buy-and-sell-stock-with-cooldown/0309-best-time-to-buy-and-sell-stock-with-cooldown.java
+++ 0309-best-time-to-buy-and-sell-stock-with-cooldown/0309-best-time-to-buy-and-sell-stock-with-cooldown.java
@@ -7,15 +7,15 @@ class Solution {
       int[][]dp=new int[n+2][2];
      
         for(int ind=n-1;ind>=0;ind--){
-            for(int buy=0;buy<=1;buy++){
-                 int profit=0;
-                if(buy==1){
-                    profit=Math.max(-prices[ind]+dp[ind+1][0],0+dp[ind+1][1]);
-                }else{
-                    profit=Math.max(prices[ind]+dp[ind+2][1],0+dp[ind+1][0]);
-                }
-                dp[ind][buy]=profit;
-            }
+                int profit=0;
+                
+                    dp[ind][1]=Math.max(-prices[ind]+dp[ind+1][0],0+dp[ind+1][1]);
+                
+                
+                    dp[ind][0]=Math.max(prices[ind]+dp[ind+2][1],0+dp[ind+1][0]);
+                
+                
+            
             
         }
         

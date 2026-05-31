2fc1afb00e0816650ed923039b768eb66c6d83c1
Arshita1510-coder <arshita1510@gmail.com>
2026-05-31 21:31:51 +0530

    Time: 26 ms (81.52%) | Memory: 107.6 MB (16.40%) - LeetSync

--- 2245-destroying-asteroids/destroying-asteroids.java
+++ 2245-destroying-asteroids/destroying-asteroids.java
@@ -1,17 +1,13 @@
 class Solution {
     public boolean asteroidsDestroyed(int mass, int[] asteroids) {
-        Arrays.sort(asteroids);
-
-        long currentMass = mass;
-
-        for (int asteroid : asteroids) {
-            if (currentMass < asteroid) {
-                return false;
-            }
-
-            currentMass += asteroid;
-        }
-
-        return true;
+       int n=asteroids.length;
+       long cmass=mass;
+       Arrays.sort(asteroids);
+       for(int i=0;i<n;i++){
+        if(cmass<asteroids[i]) return false;
+        cmass+=asteroids[i];
+       }
+       return true;
     }
+
 }

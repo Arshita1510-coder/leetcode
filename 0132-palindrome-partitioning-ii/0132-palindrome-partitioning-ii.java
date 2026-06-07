5c95c3c37f5401eba34a0a115154d3f3d936aa8d
Arshita1510-coder <arshita1510@gmail.com>
2026-06-07 21:50:35 +0530

    Time: 2499 ms (13.01%), Space: 42.9 MB (77.75%) - LeetHub

--- 0132-palindrome-partitioning-ii/0132-palindrome-partitioning-ii.java
+++ 0132-palindrome-partitioning-ii/0132-palindrome-partitioning-ii.java
@@ -8,23 +8,25 @@ class Solution {
       }
       return true;
     }
-    public static int f(int i,int n,String s,int[]dp){
-        if(i==n) return 0;
-        int minCost=Integer.MAX_VALUE;
-        if(dp[i]!=-1) return dp[i];
-        for(int j=i;j<n;j++){
-            if(isPalindrome(i,j,s,dp)){
-                int cost=1+f(j+1,n,s,dp);
-                minCost=Math.min(cost,minCost);
-            }
-        }
-        return dp[i]=minCost;
-    }
+   
     public int minCut(String s) {
         int n=s.length();
-        int[]dp=new int[n];
-        Arrays.fill(dp,-1);
-        return f(0,n,s,dp)-1;
+        int[]dp=new int[n+1];
+        dp[n]=0;
+        
+        for(int i=n-1;i>=0;i--){
+            int minCost=Integer.MAX_VALUE;
+            for(int j=i;j<n;j++){
+                if(isPalindrome(i,j,s,dp)){
+                    int cost=1+dp[j+1];
+                    minCost=Math.min(minCost,cost);
+                }
+               
+            }
+            dp[i]=minCost;
+        }
+        return dp[0]-1;
+        
 
         
     }

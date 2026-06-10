c2940bfc3d60a41b98a5711f908d5a92c71f84dc
Arshita1510-coder <arshita1510@gmail.com>
2026-06-11 01:38:49 +0530

    Time: 71 ms (38.02%), Space: 78.5 MB (25.5%) - LeetHub

--- 0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.java
+++ 0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.java
@@ -1,32 +1,25 @@
 class Solution {
-    public int largestRectangleArea(int[] heights) {
+    public static int largestRectangleArea(int[]heights){
         int n=heights.length;
         Stack<Integer>st=new Stack<>();
-        int[]leftSmall=new int[n];
-        int[]rightSmall=new int[n];
+        int maxArea=0;
         for(int i=0;i<n;i++){
-            while(!st.isEmpty()&&heights[st.peek()]>=heights[i]){
-                st.pop();
-            }
-            if(st.isEmpty()) leftSmall[i]=0;
-            else leftSmall[i]=st.peek()+1;
-            st.push(i);
-        }
-        while(!st.isEmpty()) st.pop();
-        for(int i=n-1;i>=0;i--){
             while(!st.isEmpty()&&heights[st.peek()]>heights[i]){
+                int element=st.peek();
                 st.pop();
+                int nse=i;
+                int pse=st.isEmpty()?-1:st.peek();
+                maxArea=Math.max(heights[element]*(nse-pse-1),maxArea);
             }
-            if(st.isEmpty()) rightSmall[i]=n-1;
-            else rightSmall[i]=st.peek()-1;
             st.push(i);
         }
-        int maxA=0;
-        for(int i=0;i<n;i++){
-            maxA=Math.max(maxA,heights[i]*(rightSmall[i]-leftSmall[i]+1));
+        while(!st.isEmpty()){
+            int nse=n;
+            int element=st.peek();
+            st.pop();
+            int pse=st.isEmpty()?-1:st.peek();
+            maxArea=Math.max(maxArea,(nse-pse-1)*heights[element]);
         }
-        return maxA;
-
-        
+        return maxArea;
     }
 }

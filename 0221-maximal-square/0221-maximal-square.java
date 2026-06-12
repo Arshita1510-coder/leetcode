7e3268608c8998bbe2a61db562e320bf72706be1
Arshita1510-coder <arshita1510@gmail.com>
2026-06-12 20:55:58 +0530

    Time: 56 ms (5.1%), Space: 72.5 MB (16.24%) - LeetHub

--- 0221-maximal-square/0221-maximal-square.java
+++ 0221-maximal-square/0221-maximal-square.java
@@ -1,25 +1,46 @@
 class Solution {
-    public int maximalSquare(char[][] matrix) {
-        int n=matrix.length;
-        int m=matrix[0].length;
-        int[][]dp=new int[n][m];
-        int maxSide=0;
+    public static int largestSquareArea(int[]heights){
+        int n=heights.length;
+        Stack<Integer>st=new Stack<>();
+        int maxArea=0;
         for(int i=0;i<n;i++){
-            for(int j=0;j<m;j++){
-                if(matrix[i][j]=='1'){
-                    if(i==0||j==0){
-                        dp[i][j]=1;
-                    }else{
-                        dp[i][j]=1+Math.min(dp[i-1][j],Math.min(dp[i][j-1],dp[i-1][j-1]));
-                    }
-                    maxSide=Math.max(maxSide,dp[i][j]);
-                }else{
-                    dp[i][j]=0;
-                }
+            while(!st.isEmpty()&&heights[st.peek()]>heights[i]){
+                int element=st.peek();
+                st.pop();
+                int nse=i;
+                int pse=st.isEmpty()?-1:st.peek();
+                int width=nse-pse-1;
+                int height=heights[element];
+                int side=Math.min(width,height);
+                maxArea=Math.max(maxArea,side*side);
             }
+            st.push(i);
         }
-        return maxSide*maxSide;
-
+        while(!st.isEmpty()){
+            int element=st.peek();
+            st.pop();
+            int nse=n;
+            int pse=st.isEmpty()?-1:st.peek();
+            int width=nse-pse-1;
+            int height=heights[element];
+            int side=Math.min(width,height);
+            maxArea=Math.max(maxArea,side*side);
+        }
+        return maxArea;
+    }
+    public int maximalSquare(char[][] matrix) {
+        if(matrix.length==0) return 0;
+        int m=matrix[0].length;
+        int[]heights=new int[m];
+        int maxArea=0;
+        for(char[]row:matrix){
+            for(int i=0;i<m;i++){
+                if(row[i]=='1') heights[i]++;
+                else heights[i]=0;
+            }
+            maxArea=Math.max(maxArea,largestSquareArea(heights));
+        }
+        return maxArea;
         
     }
 }

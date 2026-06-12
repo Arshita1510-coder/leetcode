88b42b6e824821d246ff70ce18d56b9adb5eeadb
Arshita1510-coder <arshita1510@gmail.com>
2026-06-13 00:54:37 +0530

    Time: 8 ms (77.05%), Space: 71 MB (40.3%) - LeetHub

--- 0221-maximal-square/0221-maximal-square.java
+++ 0221-maximal-square/0221-maximal-square.java
@@ -1,46 +1,32 @@
 class Solution {
-    public static int largestSquareArea(int[]heights){
-        int n=heights.length;
-        Stack<Integer>st=new Stack<>();
-        int maxArea=0;
-        for(int i=0;i<n;i++){
-            while(!st.isEmpty()&&heights[st.peek()]>heights[i]){
-                int element=st.peek();
-                st.pop();
-                int nse=i;
-                int pse=st.isEmpty()?-1:st.peek();
-                int width=nse-pse-1;
-                int height=heights[element];
-                int side=Math.min(width,height);
-                maxArea=Math.max(maxArea,side*side);
-            }
-            st.push(i);
-        }
-        while(!st.isEmpty()){
-            int element=st.peek();
-            st.pop();
-            int nse=n;
-            int pse=st.isEmpty()?-1:st.peek();
-            int width=nse-pse-1;
-            int height=heights[element];
-            int side=Math.min(width,height);
-            maxArea=Math.max(maxArea,side*side);
-        }
-        return maxArea;
-    }
+    
     public int maximalSquare(char[][] matrix) {
-        if(matrix.length==0) return 0;
+        int n=matrix.length;
         int m=matrix[0].length;
-        int[]heights=new int[m];
-        int maxArea=0;
-        for(char[]row:matrix){
-            for(int i=0;i<m;i++){
-                if(row[i]=='1') heights[i]++;
-                else heights[i]=0;
-            }
-            maxArea=Math.max(maxArea,largestSquareArea(heights));
+        int[][]dp=new int[n][m];
+        int maxSide=0;
+        dp[0][0]=matrix[0][0]-'0';
+        maxSide=dp[0][0];
+        for(int i=1;i<n;i++){
+            dp[i][0]=matrix[i][0]-'0';
+            maxSide=Math.max(maxSide,dp[i][0]);
         }
-        return maxArea;
+        for(int j=1;j<m;j++){
+            dp[0][j]=matrix[0][j]-'0';
+            maxSide=Math.max(maxSide,dp[0][j]);
+        }
+        for(int i=1;i<n;i++){
+            for(int j=1;j<m;j++){
+                if(matrix[i][j]=='0'){
+                    dp[i][j]=0;
+                }else{
+                    dp[i][j]=1+Math.min(dp[i-1][j],Math.min(dp[i-1][j-1],dp[i][j-1]));
+                }
+                maxSide=Math.max(maxSide,dp[i][j]);
+
+            }
+        }
+        return maxSide*maxSide;
         
     }
 }

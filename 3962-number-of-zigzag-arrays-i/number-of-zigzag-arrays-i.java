9653e250a3ab99eaa6a883c82522085fb9fcec13
Arshita1510-coder <arshita1510@gmail.com>
2026-06-24 01:22:15 +0530

    Time: 547 ms (41.03%) | Memory: 46.4 MB (89.74%) - LeetSync

--- 3962-number-of-zigzag-arrays-i/number-of-zigzag-arrays-i.java
+++ 3962-number-of-zigzag-arrays-i/number-of-zigzag-arrays-i.java
@@ -2,19 +2,13 @@ class Solution {
     public int zigZagArrays(int n, int l, int r) {
          long MOD=1000000007;
         int m = r - l + 1;
-
         long[] up = new long[m + 2];
         long[] down = new long[m + 2];
-
-        // Base case: length = 2
         for (int v = 1; v <= m; v++) {
             up[v] = v - 1;
             down[v] = m - v;
         }
-
-        // Build lengths 3..n
         for (int len = 3; len <= n; len++) {
-
             long[] prefix = new long[m + 2];
             long[] suffix = new long[m + 3];
 
@@ -28,7 +22,6 @@ class Solution {
 
             long[] newUp = new long[m + 2];
             long[] newDown = new long[m + 2];
-
             for (int v = 1; v <= m; v++) {
                 newUp[v] = prefix[v - 1];
                 newDown[v] = suffix[v + 1];
@@ -37,16 +30,13 @@ class Solution {
             up = newUp;
             down = newDown;
         }
-
         long ans = 0;
-
         if (n == 2) {
             for (int v = 1; v <= m; v++) {
                 ans = (ans + up[v] + down[v]) % MOD;
             }
             return (int) ans;
         }
-
         for (int v = 1; v <= m; v++) {
             ans = (ans + up[v] + down[v]) % MOD;
         }

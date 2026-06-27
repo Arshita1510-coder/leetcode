38e5e674866ceee68be8b892838f9603939af517
Arshita1510-coder <arshita1510@gmail.com>
2026-06-28 01:39:08 +0530

    Time: 50 ms (91.86%), Space: 67.9 MB (51.16%) - LeetHub

--- 3020-find-the-maximum-number-of-elements-in-subset/3020-find-the-maximum-number-of-elements-in-subset.java
+++ 3020-find-the-maximum-number-of-elements-in-subset/3020-find-the-maximum-number-of-elements-in-subset.java
@@ -1,36 +1,25 @@
 
 class Solution {
     public int maximumLength(int[] nums) {
-        Map<Integer, Integer> countMap = new HashMap<>();
-        
-        
+        Map<Integer, Integer> freqMap = new HashMap<>();
         for (int num : nums) {
-            countMap.put(num, countMap.getOrDefault(num, 0) + 1);
+            freqMap.put(num, freqMap.getOrDefault(num, 0) + 1);
         }
-        
         int maxLen = 1; 
-        
-       
-        if (countMap.containsKey(1)) {
-            int ones = countMap.get(1);
-            
+        if (freqMap.containsKey(1)) {
+            int ones = freqMap.get(1);
             maxLen = Math.max(maxLen, ones % 2 == 1 ? ones : ones - 1);
         }
-        
-        
-        for (int x : countMap.keySet()) {
+        for (int x : freqMap.keySet()) {
             if (x == 1) continue;
-            
             long curr = x;
             int currentLen = 0;
             
-            while (curr <= 1000000000L && countMap.containsKey((int) curr) && countMap.get((int) curr) >= 2) {
+            while (curr <= 1000000000L && freqMap.containsKey((int) curr) && freqMap.get((int) curr) >= 2) {
                 currentLen += 2;
                 curr *= curr;
             }
-            
-           
-            if (curr <= 1000000000L && countMap.containsKey((int) curr)) {
+            if (curr <= 1000000000L && freqMap.containsKey((int) curr)) {
                 
                 currentLen += 1;
             } else {

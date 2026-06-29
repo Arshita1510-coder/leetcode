ec73cb4edc49f3544ad9244f09ca29e9515814eb
Arshita1510-coder <arshita1510@gmail.com>
2026-06-30 01:39:56 +0530

    Time: 1 ms (75.62%) | Memory: 43.3 MB (74.76%) - LeetSync

--- 2099-number-of-strings-that-appear-as-substrings-in-word/number-of-strings-that-appear-as-substrings-in-word.java
+++ 2099-number-of-strings-that-appear-as-substrings-in-word/number-of-strings-that-appear-as-substrings-in-word.java
@@ -2,7 +2,7 @@ class Solution {
     public int numOfStrings(String[] patterns, String word) {
         int Count=0;
         for(String s:patterns){
-            if(word.indexOf(s)!=-1) Count++;
+            if(word.contains(s)) Count++;
         }
         return Count;
         

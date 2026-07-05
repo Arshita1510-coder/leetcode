f89970c385e3f3ed7d93056281acfe91c46843ae
Arshita1510-coder <arshita1510@gmail.com>
2026-07-05 14:04:21 +0530

    Time: 1 ms (100%), Space: 48.5 MB (13.17%) - LeetHub

--- 0977-squares-of-a-sorted-array/0977-squares-of-a-sorted-array.java
+++ 0977-squares-of-a-sorted-array/0977-squares-of-a-sorted-array.java
@@ -1,67 +1,75 @@
 class Solution {
     public int[] sortedSquares(int[] nums) {
-        int n = nums.length;
-        List<Integer> neg = new ArrayList<>();
-        List<Integer> pos = new ArrayList<>();
-
-        // Separate negative and positive numbers
-        for (int num : nums) {
-            if (num < 0)
-                neg.add(num);
-            else
-                pos.add(num);
-        }
-
-        // Case 1: No negative numbers
-        if (neg.size() == 0) {
-            for (int i = 0; i < pos.size(); i++)
-                pos.set(i, pos.get(i) * pos.get(i));
-            return pos.stream().mapToInt(Integer::intValue).toArray();
-        }
-
-        // Case 2: No positive numbers
-        if (pos.size() == 0) {
-            for (int i = 0; i < neg.size(); i++)
-                neg.set(i, neg.get(i) * neg.get(i));
-            Collections.reverse(neg);
-            return neg.stream().mapToInt(Integer::intValue).toArray();
-        }
-
-        // Case 3: Both negative and positive exist
-        int i = 0, j = 0, id = 0;
-        int n1 = neg.size();
-        int n2 = pos.size();
-        int[] res = new int[n1 + n2];
-
-        // Square negatives and reverse them
-        for (i = 0; i < n1; i++)
-            neg.set(i, neg.get(i) * neg.get(i));
-        Collections.reverse(neg);
-
-        // Square positives
-        for (i = 0; i < n2; i++)
-            pos.set(i, pos.get(i) * pos.get(i));
-
-        // Merge two sorted lists
-        i = 0; // j=0
-        j=0;
-        while (i < n1 && j < n2) {
-            if (neg.get(i) <= pos.get(j)) {
-                res[id++] = neg.get(i++);
-                // res[id]=neg[i]
-                // id++;
-                // i++
-            } else {
-                res[id++] = pos.get(j++);
+        int n=nums.length;
+        int negCount=0;
+        int posCount=0;
+        for(int num:nums){
+            if(num<0){
+                negCount++;
+            }else{
+                posCount++;
             }
-        } // while loop khatam
-
-        while (i < n1)
-            res[id++] = neg.get(i++);
-
-        while (j < n2)
-            res[id++] = pos.get(j++);
-
-        return res;
+        }
+        if(negCount==0){
+            int[]ans=new int[n];
+            for(int i=0;i<n;i++){
+                ans[i]=nums[i]*nums[i];
+            }
+            return ans;
+        }
+        if(posCount==0){
+            int[]ans=new int[n];
+            int k=0;
+            for(int i=n-1;i>=0;i--){
+                ans[k++]=nums[i]*nums[i];
+            }
+            return ans;
+            
+        }
+        int[]neg=new int[negCount];
+        int[]pos=new int[posCount];
+        int x=0;
+        int y=0;
+        for(int num:nums){
+            if(num<0){
+                neg[x++]=num;
+            }else{
+                pos[y++]=num;
+            }
+        }
+        for(int i=0;i<negCount;i++){
+            neg[i]=neg[i]*neg[i];
+        }
+        int start=0;
+        int end=negCount-1;
+        while(start<end){
+            int temp=neg[start];
+            neg[start]=neg[end];
+            neg[end]=temp;
+            start++;
+            end--;
+        }
+        for(int i=0;i<posCount;i++){
+            pos[i]=pos[i]*pos[i];
+        }
+        int i=0;
+        int j=0;
+        int k=0;
+        int[]ans=new int[n];
+        while(i<negCount&&j<posCount){
+            if(neg[i]<=pos[j]){
+                ans[k++]=neg[i++];
+            }else{
+                ans[k++]=pos[j++];
+            }
+        }
+        while(i<negCount){
+            ans[k++]=neg[i++];
+        }
+        while(j<posCount){
+            ans[k++]=pos[j++];
+        }
+        return ans;
+        
     }
 }

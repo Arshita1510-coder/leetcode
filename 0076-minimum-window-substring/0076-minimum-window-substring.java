9fcf57c351d74fc26fb0a3642dd3d9f452bfbc1c
Arshita1510-coder <arshita1510@gmail.com>
2026-07-21 15:13:48 +0530

    Time: 2 ms (99.82%), Space: 45.6 MB (76.66%) - LeetHub

--- 0076-minimum-window-substring/0076-minimum-window-substring.java
+++ 0076-minimum-window-substring/0076-minimum-window-substring.java
@@ -1,35 +1,29 @@
 class Solution {
     public String minWindow(String s, String t) {
-        int m=t.length();
-        int n=s.length();
-        if(m>n)  return "";
-        int[]hash=new int[256];
-        for(int i=0;i<m;i++){
-            hash[t.charAt(i)]++;
-        }
-        int Count=0;
-        int minLen=Integer.MAX_VALUE;
-        int startIndex=-1;
-        int left=0;
-        for(int right=0;right<n;right++){
-            if(hash[s.charAt(right)]>0){
-                Count++;
+       int minLen=Integer.MAX_VALUE;
+       int[]hash=new int[256];
+       int left=0;
+       int right=0;
+       int sIndex=-1;
+       int m=t.length();
+       int n=s.length();
+       for(int i=0;i<m;i++) hash[t.charAt(i)]++;
+       int Cnt=0;
+       while(right<n){
+        if(hash[s.charAt(right)]>0) Cnt+=1;
+        hash[s.charAt(right)]--;
+        while(Cnt==m){
+            if(right-left+1<minLen){
+                minLen=right-left+1;
+                sIndex=left;
             }
-            hash[s.charAt(right)]--;
-            while(Count==m){
-                if(right-left+1<minLen){
-                    minLen=right-left+1;
-                    startIndex=left;
-                }
-                hash[s.charAt(left)]++;
-                if(hash[s.charAt(left)]>0){
-                    Count--;
-                }
-                left++;
-            }
-        }
-        return (startIndex==-1)?"":s.substring(startIndex,startIndex+minLen);
-
-        
+            hash[s.charAt(left)]++;
+            
+            if(hash[s.charAt(left)]>0) Cnt--;
+            left++;
+        } 
+        right++;
+       }
+       return sIndex==-1?"":s.substring(sIndex,sIndex+minLen);
     }
 }

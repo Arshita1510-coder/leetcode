b83a20e2bc306a8d71972cc256a381a115905da7
Arshita1510-coder <arshita1510@gmail.com>
2026-08-02 19:27:47 +0530

    Time: 1 ms (99.51%) | Memory: 42.8 MB (49.58%) - LeetSync

---
+++ 1537-maximum-score-after-splitting-a-string/maximum-score-after-splitting-a-string.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int maxScore(String s) {
+        int rightOnes=0;
+        for(char ch:s.toCharArray()){
+            if(ch=='1') rightOnes++;
+
+        }
+        int leftZeros=0;
+        int maxScore=0;
+        for(int i=0;i<s.length()-1;i++){
+            if(s.charAt(i)=='0') leftZeros++;
+            else rightOnes--;
+            int score=leftZeros+rightOnes;
+            maxScore=Math.max(score,maxScore);
+        }
+        return maxScore;
+
+
+        
+    }
+}

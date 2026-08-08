10f45446cad95b03a12b85cfa1f1160d7a68f8c8
Arshita1510-coder <arshita1510@gmail.com>
2026-08-09 00:45:49 +0530

    Time: 3 ms (99.32%), Space: 47.4 MB (39.23%) - LeetHub

--- 0986-interval-list-intersections/0986-interval-list-intersections.java
+++ 0986-interval-list-intersections/0986-interval-list-intersections.java
@@ -10,11 +10,21 @@ class Solution {
             int end1=firstList[i][1];
             int start2=secondList[j][0];
             int end2=secondList[j][1];
-            int s=Math.max(start1,start2);
-             int e=Math.min(end1,end2);
-             if(s<=e){
-                res.add(new int[]{s,e});
-             }
+            if(start1<=start2){
+                if(end1>=start2){
+                    int s=Math.max(start1,start2);
+                    int e=Math.min(end1,end2);
+                    res.add(new int[]{s,e});
+                }
+            }else{
+                if(end2>=start1){
+                        int s=Math.max(start1,start2);
+                        int e=Math.min(end1,end2);
+                        res.add(new int[]{s,e});
+
+                }
+            }
+        
 
                 
                 

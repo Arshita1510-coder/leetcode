055015f333daae6c5ad622db4a84c77b1b654a39
Arshita1510-coder <arshita1510@gmail.com>
2026-08-23 20:38:59 +0530

    Time: 25 ms (40.47%), Space: 48.5 MB (26.32%) - LeetHub

--- 0227-basic-calculator-ii/0227-basic-calculator-ii.java
+++ 0227-basic-calculator-ii/0227-basic-calculator-ii.java
@@ -1,5 +1,6 @@
 class Solution {
     public int calculate(String s) {
+        if(s==null||s.length()==0) return 0;
         Stack<Integer>st=new Stack<>();
         int num=0;
         char sign='+';

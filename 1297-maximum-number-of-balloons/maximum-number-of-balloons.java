4b3cc5afcb6be569fefd8586e8d9e0fcc65c4bbe
Arshita1510-coder <arshita1510@gmail.com>
2026-08-27 02:32:34 +0530

    Time: 11 ms (26.02%) | Memory: 44.5 MB (7.00%) - LeetSync

--- 1297-maximum-number-of-balloons/maximum-number-of-balloons.java
+++ 1297-maximum-number-of-balloons/maximum-number-of-balloons.java
@@ -1,13 +1,22 @@
 class Solution {
     public int maxNumberOfBalloons(String text) {
-        int[]freq=new int[26];
+        HashMap<Character,Integer>textFreq=new HashMap<>();
         for(char ch:text.toCharArray()){
-            freq[ch-'a']++;
+            textFreq.put(ch,textFreq.getOrDefault(ch,0)+1);
         }
-        return Math.min(Math.min(freq['b'-'a'],freq['a'-'a']),Math.min(Math.min(freq['l'-'a']/2,freq['o'-'a']/2),freq['n'-'a']));
+        HashMap<Character,Integer>balloonFreq=new HashMap<>();
+        String balloon="balloon";
+        for(char ch:balloon.toCharArray()){
+            balloonFreq.put(ch,balloonFreq.getOrDefault(ch,0)+1);
+        }
+        int res=Integer.MAX_VALUE;
+        for(char ch:balloonFreq.keySet()){
+            int have=textFreq.getOrDefault(ch,0);
+            int need=balloonFreq.get(ch);
+            res=Math.min(res,have/need);
+        }
+        return res;
 
-
-        
         
     }
 }

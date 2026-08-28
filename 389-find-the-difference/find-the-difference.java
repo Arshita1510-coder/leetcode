62324ba9cfcd14da9eb8b5d9889e853664888d26
Arshita1510-coder <arshita1510@gmail.com>
2026-08-28 17:46:03 +0530

    Time: 2 ms (66.04%) | Memory: 42.8 MB (96.76%) - LeetSync

---
+++ 389-find-the-difference/find-the-difference.java
@@ -0,0 +1,13 @@
+class Solution {
+    public char findTheDifference(String s, String t) {
+        int Sum=0;
+        for(int i=0;i<t.length();i++){
+            Sum^=t.charAt(i);
+        }
+        for(int i=0;i<s.length();i++){
+            Sum^=s.charAt(i);
+        }
+        return(char) Sum;
+        
+    }
+}

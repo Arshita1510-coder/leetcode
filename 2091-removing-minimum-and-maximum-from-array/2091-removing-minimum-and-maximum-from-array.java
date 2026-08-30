4bd700e4ed402e2bc5ae7d8f6776156be61d12d1
Arshita1510-coder <arshita1510@gmail.com>
2026-08-31 03:24:45 +0530

    Time: 3 ms (62.18%), Space: 86.6 MB (60.18%) - LeetHub

--- 2091-removing-minimum-and-maximum-from-array/2091-removing-minimum-and-maximum-from-array.java
+++ 2091-removing-minimum-and-maximum-from-array/2091-removing-minimum-and-maximum-from-array.java
@@ -3,6 +3,7 @@ class Solution {
         int n=nums.length;
         int minIndex=0;
         int maxIndex=0;
+        if(n<=2) return n;
         for(int i=0;i<n;i++){
             if(nums[i]<nums[minIndex]){
                 minIndex=i;

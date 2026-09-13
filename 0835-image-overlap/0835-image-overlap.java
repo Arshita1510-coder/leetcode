5bac4274dba73709cbad883b991b6277c36c72a1
Arshita1510-coder <arshita1510@gmail.com>
2026-09-14 04:06:04 +0530

    Time: 181 ms (24.72%), Space: 48 MB (6.46%) - LeetHub

--- 0835-image-overlap/0835-image-overlap.java
+++ 0835-image-overlap/0835-image-overlap.java
@@ -1,26 +1,45 @@
 class Solution {
     public int largestOverlap(int[][] img1, int[][] img2) {
-        int n=img1.length;
-        HashMap<String,Integer>map=new HashMap<>();
-        int ans=0;
-        for(int r1=0;r1<n;r1++){
-            for(int c1=0;c1<n;c1++){
-                if(img1[r1][c1]==1){
-                    for(int r2=0;r2<n;r2++){
-                        for(int c2=0;c2<n;c2++){
-                            if(img2[r2][c2]==1){
-                                int dr=r2-r1;
-                                int dc=c2-c1;
-                                String key=dr+","+dc;
-                                map.put(key,map.getOrDefault(key,0)+1);
-                                ans=Math.max(ans,map.get(key));
-                            }
-                        }
-                    }
+
+        int n = img1.length;
+
+        ArrayList<int[]> a = new ArrayList<>();
+        ArrayList<int[]> b = new ArrayList<>();
+
+        // Store only positions of 1s
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+
+                if (img1[i][j] == 1) {
+                    a.add(new int[]{i, j});
+                }
+
+                if (img2[i][j] == 1) {
+                    b.add(new int[]{i, j});
                 }
             }
         }
+
+        HashMap<String, Integer> map = new HashMap<>();
+
+        int ans = 0;
+
+        for (int[] p1 : a) {
+            for (int[] p2 : b) {
+
+                int dr = p2[0] - p1[0];
+                int dc = p2[1] - p1[1];
+
+                String key = dr + "," + dc;
+
+                int freq = map.getOrDefault(key, 0) + 1;
+
+                map.put(key, freq);
+
+                ans = Math.max(ans, freq);
+            }
+        }
+
         return ans;
-        
     }
 }
